f760017dedc6a35fe8e070e35ccb891be368ce77
crised <crised@gmail.com>
2015-10-29 11:00:31 -0300

    feat added content descriptors

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
@@ -1,10 +1,15 @@
 package barqsoft.footballscores.widget;
 
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.widget.RemoteViews;
 
+import barqsoft.footballscores.MainActivity;
 import barqsoft.footballscores.R;
 
 /**
@@ -12,17 +17,29 @@ import barqsoft.footballscores.R;
  */
 public class TodayWidgetProvider extends AppWidgetProvider {
 
+    //Gets Called on OnReceive() //UI Thread
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
         for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_today_small);
 
-            views.setTextViewText(R.id.widget_score_1, "1-0");
+            views.setTextViewText(R.id.widget_score_1, "Today!");
+            views.setImageViewResource(R.id.widget_icon, R.drawable.ic_launcher);
 
+            //Intent to launch MainActivity
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            //Update Widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
 
+    }
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+        views.setContentDescription(R.id.widget_icon, description);
     }
 }
